5d493734384e4e52094620d89d25165a446156ee
kevin <0912293@hr.nl>
2018-03-23 12:03:21 +0100

    can send request but cant read it yet

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class MainUi(QMainWindow, homeScreen.Ui_MainWindow):
         super(MainUi, self).__init__(parent)
         self.setupUi(self)
         self.initUI()
+        self.popTable()
 
 
     def initUI(self):
@@ -26,17 +27,24 @@ class MainUi(QMainWindow, homeScreen.Ui_MainWindow):
         self.label.setToolTip("Label 1")
         self.label_2.setToolTip("Label 2")
         self.pushButton.setText("Schedule")
-        self.pushButton.clicked.connect(self.popTable)
-        self.pushButton_2.setText   ("Defects")
+        self.pushButton.clicked.connect(self.buttonClicked)
+        self.pushButton_2.setText("Defects")
         self.label.setText("capacity")
         self.label_2.setText("temp")
 
-
     def popTable(self):
-        contents = urllib.request.urlopen("http://markb.pythonanywhere.com/getdata").read() #b'{"Reservation": {"ReservationID": 10, "TimeSlotFrom": 2, "TimeSlotTo": 3, "TimeFrom": "10:50", "TimeTo": "11:40", "Date": "12-03-2003", "Username": "OmarA", "Lesson": "ICTLAB", "Room": "WN4.101"}}'
-        data = json.load(contents)
-        self.tableWidget.setItem(int(data["Reservation"]["TimeSlotFrom"]), 0, QTableWidgetItem(str(data["Reservation"]["TimeFrom"])+ str(data["Reservation"]["TimeTo"]) + str(data["Reservation"]["Lesson"]) + str(data["Reservation"]["ICTLAB"])))
+        for i in range(15):
+            self.tableWidget.setItem(i,0, QTableWidgetItem(str(i+1)))
+        contents = urllib.request.urlopen(
+            "http://markb.pythonanywhere.com/getdata").read()  # b'{"Reservation": {"ReservationID": 10, "TimeSlotFrom": 2, "TimeSlotTo": 3, "TimeFrom": "10:50", "TimeTo": "11:40", "Date": "12-03-2003", "Username": "OmarA", "Lesson": "ICTLAB", "Room": "WN4.101"}}'
+        data = json.loads(contents.decode('utf-8'))
+        print(json.dumps(data, indent=4, sort_keys=True))
+        self.insertTable(data)
 
+    def insertTable(self, data):
+        self.tableWidget.setItem(int(data["Reservation"]["TimeSlotFrom"]), 1, QTableWidgetItem(
+            str(data["Reservation"]["TimeFrom"]) + " " + str(data["Reservation"]["TimeTo"]) + " " + str(
+                data["Reservation"]["Username"]) + " " + str(data["Reservation"]["Lesson"])))
 
     def closeEvent(self, event):
         reply = QMessageBox.question(self, 'Message',
